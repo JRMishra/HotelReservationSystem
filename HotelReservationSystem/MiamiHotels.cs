d688855656cced8241ec6940e0a140853da2a570
JR Mishra <jrmnitdgp@gmail.com>
2020-10-31 23:26:25 +0530

    [JR Mishra]Test. Passed all positive and negative test situations for UC1

--- HotelReservationSystem/MiamiHotels.cs
+++ HotelReservationSystem/MiamiHotels.cs
@@ -11,24 +11,50 @@ namespace HotelReservationSystem
 
         public static Dictionary<string, Hotel> HotelList { get => hotelList; }
 
+        /// <summary>
+        /// Take hotel name and rate, if eligible, add to hotel reservation system
+        /// </summary>
+        /// <param name="hotelName">Name of hotel</param>
+        /// <param name="dailyRate">Daily Rate of hotel</param>
         public static void AddHotel(string hotelName, double dailyRate)
         {
-            if (hotelName == null)
-                throw new HotelReservationException(HotelReservationException.ExceptionType.NULL_HOTEL_NAME, "Hotel name can not be null");
-           
-            if (dailyRate == 0.0)
-                throw new HotelReservationException(HotelReservationException.ExceptionType.ZERO_HOTEL_RATE, "Hotel rate can not be null");
-            
-            if(hotelList.ContainsKey(hotelName))
+            Hotel hotel = new Hotel(hotelName, dailyRate);
+            if (IsEligible(hotel))
+                hotelList.Add(hotel.Name, hotel);
+            else
                 throw new HotelReservationException(HotelReservationException.ExceptionType.HOTEL_ALREADY_EXISTS, "This hotel already exist");
 
-            Hotel hotel = new Hotel(hotelName, dailyRate);
-            hotelList.Add(hotelName, hotel);
         }
 
+        /// <summary>
+        /// Take a hotel, if eligible, add to hotel reservation system
+        /// </summary>
+        /// <param name="hotel">hotel to add in reservation system</param>
         public static void AddHotel(Hotel hotel)
         {
-            AddHotel(hotel.Name, hotel.Rates);
+            if(IsEligible(hotel))
+                hotelList.Add(hotel.Name, hotel);
+            else
+                throw new HotelReservationException(HotelReservationException.ExceptionType.HOTEL_ALREADY_EXISTS, "This hotel already exist");
+        }
+
+        /// <summary>
+        /// Check eligibility criteria for hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns>Returns true if eligible, else false</returns>
+        private static bool IsEligible(Hotel hotel)
+        {
+            if (hotel.Name == null)
+                throw new HotelReservationException(HotelReservationException.ExceptionType.NULL_HOTEL_NAME, "Hotel name can not be null");
+
+            if (hotel.Rates == 0.0)
+                throw new HotelReservationException(HotelReservationException.ExceptionType.ZERO_HOTEL_RATE, "Hotel rate can not be null");
+
+            if (!hotelList.ContainsKey(hotel.Name))
+                return true;
+            else
+                return false;
         }
     }
 }
--- TestHotelReservationSystem/UnitTest1.cs
+++ TestHotelReservationSystem/UnitTest1.cs
@@ -6,7 +6,151 @@ namespace TestHotelReservationSystem
 {
     [TestClass]
     public class UnitTest1
-    {   
-        
+    {
+        /// <summary>
+        /// Given name and rate of hotel, 
+        /// it should create an instance of hotel class with corresponding properties 
+        /// </summary>
+        [TestMethod]
+        public void GivenHotelNameAndRate_ShouldCreateNewHotelObject()
+        {
+            //Arrange
+            Hotel hotel;
+            string expectedName = "LakeWood";
+            double expectedRate = 90;
+
+            //Act
+            hotel = new Hotel(expectedName, expectedRate);
+
+            //Assert
+            Assert.AreEqual(expectedName, hotel.Name);
+            Assert.AreEqual(expectedRate, hotel.Rates);
+        }
+
+        /// <summary>
+        /// Given hotel name and rate, 
+        /// it should add it to hotel reservation system
+        /// </summary>
+        [TestMethod]
+        public void GivenNameAndRate_ShouldAddHotelToResvSystem()
+        {
+            //Arrange
+            string expectedName = "LakeWood";
+            double expectedRate = 90;
+
+            //Act
+            MiamiHotels.HotelList.Clear();
+            MiamiHotels.AddHotel(expectedName, expectedRate);
+
+            //Assert
+            Assert.AreEqual(expectedName, MiamiHotels.HotelList[expectedName].Name);
+            Assert.AreEqual(expectedRate, MiamiHotels.HotelList[expectedName].Rates);
+        }
+
+        /// <summary>
+        /// Given a proper hotel,
+        /// it should add it to hotel reservation system
+        /// </summary>
+        [TestMethod]
+        public void GivenHotel_ShouldAddToReservationSystem()
+        {
+            //Arrange
+            string expectedName = "BridgeWood";
+            double expectedRate = 100;
+            Hotel hotel = new Hotel(expectedName, expectedRate);
+
+            //Act
+            MiamiHotels.HotelList.Clear();
+            MiamiHotels.AddHotel(hotel);
+
+            //Assert
+            Assert.AreEqual(hotel.GetType(), MiamiHotels.HotelList[expectedName].GetType());
+        }
+
+        /// <summary>
+        /// Given hotel name as Null,
+        /// it should throw custom exception with appropriate message
+        /// </summary>
+        [TestMethod]
+        public void GivenNullHotelName_ShouldThrowCustomException()
+        {
+            //Arrange
+            Hotel hotel = new Hotel(null, 100);
+            Exception exception = new Exception();
+            //Act
+            try
+            {
+                MiamiHotels.HotelList.Clear();
+                MiamiHotels.AddHotel(hotel);
+            }
+            catch (HotelReservationException e)
+            {
+                exception = e;
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+            //Assert
+            Assert.AreEqual(exception.Message, "Hotel name can not be null");
+        }
+
+        /// <summary>
+        /// Given hotel rate as zero,
+        /// it should throw custom exception with appropriate message
+        /// </summary>
+        [TestMethod]
+        public void GivenZeroHotelRate_ShouldThrowCustomException()
+        {
+            //Arrange
+            Hotel hotel = new Hotel("RidgeWood", 0);
+            Exception exception = new Exception();
+            //Act
+            try
+            {
+                MiamiHotels.HotelList.Clear();
+                MiamiHotels.AddHotel(hotel);
+            }
+            catch (HotelReservationException e)
+            {
+                exception = e;
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+            //Assert
+            Assert.AreEqual(exception.Message, "Hotel rate can not be null");
+        }
+
+        /// <summary>
+        /// If we try to add a hotel more than once
+        /// it should throw custom exception with appropriate message
+        /// </summary>
+        [TestMethod]
+        public void GivenDuplicateHotel_ShouldThrowCustomException()
+        {
+            //Arrange
+            Hotel firstHotel = new Hotel("LakeWood", 100);
+            Hotel secondHotel = new Hotel("LakeWood", 120);
+            Exception exception = new Exception();
+            //Act
+            try
+            {
+                MiamiHotels.HotelList.Clear();
+                MiamiHotels.AddHotel(firstHotel);
+                MiamiHotels.AddHotel(secondHotel);
+            }
+            catch (HotelReservationException e)
+            {
+                exception = e;
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+            //Assert
+            Assert.AreEqual(exception.Message, "This hotel already exist");
+        }
     }
 }
